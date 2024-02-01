cfeb0e90c251f908421b8f672aaa27029cce8729
Luis Padron <luispadron25@gmail.com>
2024-01-31 19:26:57 -0500

    Reto 17 main, readme, unit test added

---
+++ retos/reto17/reto17.test.js
@@ -0,0 +1,95 @@
+const optimizeIntervals = require("./main");
+
+test("Test #01 - Returns an Array", () => {
+  expect(
+    Array.isArray(
+      optimizeIntervals([
+        [2, 7],
+        [3, 4],
+        [5, 8],
+      ])
+    )
+  ).toBe(true);
+});
+
+test("Test #02 - optimizeIntervals([[2, 7], [3, 4], [5, 8]])", () => {
+  expect(
+    optimizeIntervals([
+      [2, 7],
+      [3, 4],
+      [5, 8],
+    ])
+  ).toStrictEqual([[2, 8]]);
+});
+
+test("Test #03 - optimizeIntervals([[3, 4], [5, 8], [2, 7]])", () => {
+  expect(
+    optimizeIntervals([
+      [3, 4],
+      [5, 8],
+      [2, 7],
+    ])
+  ).toStrictEqual([[2, 8]]);
+});
+
+test("Test #04 - optimizeIntervals([[1, 3], [2, 6], [8, 10]])", () => {
+  expect(
+    optimizeIntervals([
+      [1, 3],
+      [2, 6],
+      [8, 10],
+    ])
+  ).toStrictEqual([
+    [1, 6],
+    [8, 10],
+  ]);
+});
+
+test("Test #05 - optimizeIntervals([[1, 2], [3, 4], [5, 6]])", () => {
+  expect(
+    optimizeIntervals([
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ])
+  ).toStrictEqual([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ]);
+});
+
+test("Test #06 - optimizeIntervals([[5, 7], [6, 8]])", () => {
+  expect(
+    optimizeIntervals([
+      [5, 7],
+      [6, 8],
+    ])
+  ).toStrictEqual([[5, 8]]);
+});
+
+test("Test #07 - optimizeIntervals([[1, 5], [6, 10], [11, 15], [16, 20]])", () => {
+  expect(
+    optimizeIntervals([
+      [1, 5],
+      [6, 10],
+      [11, 15],
+      [16, 20],
+    ])
+  ).toStrictEqual([
+    [1, 5],
+    [6, 10],
+    [11, 15],
+    [16, 20],
+  ]);
+});
+
+test("Test #08 - optimizeIntervals([[1, 15], [8, 12], [4, 7]])", () => {
+  expect(
+    optimizeIntervals([
+      [1, 15],
+      [8, 12],
+      [4, 7],
+    ])
+  ).toStrictEqual([[1, 15]]);
+});
